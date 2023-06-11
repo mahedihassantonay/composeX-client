e4718baaaba9d06f5862004120f8aa2dea5e03cd
mahedihassantonay <mahedihassantonay@gmail.comm>
2023-06-11 14:00:00 +0600

    course added to my selectred course

--- src/pages/Classes/Classes.jsx
+++ src/pages/Classes/Classes.jsx
@@ -6,9 +6,9 @@ const Classes = () => {
   const [courses, setCourses] = useState([]);
   const [selectedCourses, setSelectedCourses] = useState([]);
 
-  const {user} = useAuth()
+  const { user } = useAuth();
   const navigate = useNavigate();
-  const location = useLocation()
+  const location = useLocation();
 
   useEffect(() => {
     fetch("http://localhost:5000/classes")
@@ -28,57 +28,57 @@ const Classes = () => {
     }
   }, [user]);
 
-
-  const handleSelectedClass = course =>{
-    console.log(course)
-    if(user && user.email){
-      const selectedClass={classId: course._id, price: course.price, image: course.image,  instructor: course.instructor, name: course.className , email: user?.email }
-      fetch('http://localhost:5000/selectedClasses',{
-        method: 'POST',
+  const handleSelectedClass = (course) => {
+    console.log(course);
+    if (user && user.email) {
+      const selectedClass = {
+        classId: course._id,
+        price: course.price,
+        image: course.image,
+        instructor: course.instructor,
+        name: course.className,
+        email: user?.email,
+        instructorImage: course.instractorImage,
+        seats: course.availableSeats
+      };
+      fetch("http://localhost:5000/selectedClasses", {
+        method: "POST",
         headers: {
-            'content-type': 'application/json',
-
+          "content-type": "application/json",
         },
-        body: JSON.stringify(selectedClass)
-
-    })
-      .then(res=>res.json())
-      .then(data=>{
-        if(data.insertedId){
-          alert('added suiccesfully')
-          setSelectedCourses([...selectedCourses, course._id]);
-
-        }else{
-          navigate('/login', {state: {from: location}})
-        }
+        body: JSON.stringify(selectedClass),
       })
-
+        .then((res) => res.json())
+        .then((data) => {
+          if (data.insertedId) {
+            alert("added suiccesfully");
+            setSelectedCourses([...selectedCourses, course._id]);
+          } else {
+            navigate("/login", { state: { from: location } });
+          }
+        });
     }
-  }
+  };
   return (
     <div className="grid gap-8 m-12">
-      {
-        courses.map(course=>(
-            <div key={course.key} className="hero bg-base-200 flex flex-row">
-        <div className="hero-content px-12">
-          <img
-            src={course.image}
-            className="max-w-sm rounded-lg shadow-2xl"
-          />
+      {courses.map((course) => (
+        <div key={course.key} className="hero bg-base-200 flex flex-row">
+          <div className="hero-content px-12">
+            <img
+              src={course.image}
+              className="max-w-sm rounded-lg shadow-2xl"
+            />
           </div>
           <div className="">
             <h1 className="text-5xl font-bold">{course.className}</h1>
-            <p className="py-6">
-              Instructor: {course.instructor}
-            </p>
-            <p className="py-6">
-              Available Seats: {course.availableSeats}
-            </p>
-            <p className="py-6">
-              Price: ${course.price}
-            </p>
+            <p className="py-6">Instructor: {course.instructor}</p>
+            <p className="py-6">Available Seats: {course.availableSeats}</p>
+            <p className="py-6">Price: ${course.price}</p>
             {selectedCourses.includes(course._id) ? (
-              <button className="btn bg-gray-400 text-white cursor-not-allowed" disabled>
+              <button
+                className="btn bg-gray-400 text-white cursor-not-allowed"
+                disabled
+              >
                 Selected
               </button>
             ) : (
@@ -92,10 +92,8 @@ const Classes = () => {
               </Link>
             )}
           </div>
-        
-      </div>
-        ))
-      }
+        </div>
+      ))}
     </div>
   );
 };
--- src/pages/Dashboard/MySelectedClass.jsx
+++ src/pages/Dashboard/MySelectedClass.jsx
@@ -1,11 +1,44 @@
 import useSelectedClass from "../../hooks/useSelectedClass";
 
 const MySelectedClass = () => {
-    const [course] = useSelectedClass()
+    const [course, refetch] = useSelectedClass()
+    const handleDelete = course =>{
+        
+    }
     return (
-        <div>
-            <h1>my selecyted class: {course.length}</h1>
+        <>
+        <div className="flex flex-col gap-12">
+            {
+                course.map(c=> (
+                    <div key={c._id} className="mx-8">
+            <div className="rounded-lg  bg-base-200">
+  <div className="flex gap-12 justify-between items-center">
+  <div className="w-2/3 p-4">
+  <h1 className="text-xl font-bold mb-4">Course Name: {c.name}</h1> <br />
+    <img src={c.image}className="h-52 w-96 rounded-lg shadow-2xl" />
+  </div>
+    <div className="w-1/3">
+        <h1 className="text-lg font-bold mb-4">Instructor: <br /> {c.instructor}</h1>
+        <img className="h-52  rounded-lg" src={c.instructorImage} alt="" />
+    </div>
+    </div>
+    <div className="p-4">
+      <h1 className="text-3xl font-bold">${c.price}</h1>
+      <p className="py-6 text-yellow-600">Available Seat: {c.seats}</p>
+      <div>
+      <button onClick={()=>handleDelete(item)} className="btn bg-black text-white hover:text-black">Delete</button>
+      <button className="btn bg-black text-white hover;text-black">Pay</button>
+      </div>
+    </div>
+  
+</div>
+
         </div>
+                ))
+            }
+        </div>
+        
+        </>
     );
 };
 
